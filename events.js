ec2dbd8f4b23705fa247f4a6cf0772a311d97d20
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-08-23 15:50:34 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,19 @@
+function changecolor() {
+    document.getElementById("abc").style.backgroundColor = "blue"
+}
+function input() {
+    document.getElementById("xyz").style.backgroundColor = "maroon"
+}
+function display() {
+    document.getElementById("abcd").style.backgroundColor = "yellow"
+}
+function change() {
+    document.getElementById("kpj").style.backgroundColor = "green"
+}
+function input1() {
+    document.getElementById("asd").style.backgroundColor = "red"
+}
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
